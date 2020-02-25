028d2fbe9d724d33df42b1adea33193dfca5fa4c
Walkyst <vles491@gmail.com>
2020-02-25 21:23:41 +0300

    okay, then i just check ids

--- sentinel/src/main/java/com/fredboat/sentinel/jda/JdaRabbitEventListener.kt
+++ sentinel/src/main/java/com/fredboat/sentinel/jda/JdaRabbitEventListener.kt
@@ -227,8 +227,8 @@ class JdaRabbitEventListener(
                 event.messageIdLong,
                 event.guild.idLong,
                 event.channel.idLong,
-                event.reactionEmote.idLong,
-                event.member.user.isBot
+                event.user.idLong,
+                event.reactionEmote.idLong
         ))
     }
 
