2d8fec0da9adc730fbcbe1d8edfc99f35ded1716
Walkyst <41806921+Walkyst@users.noreply.github.com>
2022-07-28 17:13:43 +0300

    Voice, Text and Role event updates

--- sentinel-shared/src/main/java/com/fredboat/sentinel/entities/guildSync.kt
+++ sentinel-shared/src/main/java/com/fredboat/sentinel/entities/guildSync.kt
@@ -9,19 +9,19 @@ package com.fredboat.sentinel.entities
 
 /** Returns [Guild] as well as jda */
 data class GuildSubscribeRequest(
-        val id: String,
-        val requestTime: String,
-        val shardId: String,
-        /** Optionally a channel to post a warning in, if the guild is very large */
-        val channelInvoked: String?,
-        val responseId: String
+    val id: String,
+    val requestTime: String,
+    val shardId: String,
+    /** Optionally a channel to post a warning in, if the guild is very large */
+    val channelInvoked: String?,
+    val responseId: String
 )
 
 /** Sent when the [Guild] gets uncached */
 data class GuildUnsubscribeRequest(val id: String)
 
 data class GuildUpdateEvent(
-        val guild: Guild
+    val guild: Guild
 )
 
 /* Updates */
@@ -29,26 +29,56 @@ data class GuildUpdateEvent(
 
 /** When we are subscribed and one of the members change (presence, name, etc) */
 data class GuildMemberUpdate(
-        val guild: String,
-        val member: Member
+    val guild: String,
+    val member: Member
+)
+
+data class RoleCreate(
+    val guild: String,
+    val role: Role
+)
+
+data class RoleDelete(
+    val guild: String,
+    val role: Role
 )
 
 data class RoleUpdate(
-        val guild: String,
-        val role: Role
+    val guild: String,
+    val role: Role
+)
+
+data class TextChannelCreate(
+    val guild: String,
+    val channel: TextChannel
+)
+
+data class TextChannelDelete(
+    val guild: String,
+    val channel: TextChannel
 )
 
 data class TextChannelUpdate(
-        val guild: String,
-        val channel: TextChannel
+    val guild: String,
+    val channel: TextChannel
+)
+
+data class VoiceChannelCreate(
+    val guild: String,
+    val channel: VoiceChannel
+)
+
+data class VoiceChannelDelete(
+    val guild: String,
+    val channel: VoiceChannel
 )
 
 data class VoiceChannelUpdate(
-        val guild: String,
-        val channel: VoiceChannel
+    val guild: String,
+    val channel: VoiceChannel
 )
 
 data class ChannelPermissionsUpdate(
-        val guild: String,
-        val changes: Map<String, String>
+    val guild: String,
+    val changes: Map<String, String>
 )
